b1d01514c240b6c1315d95ebda869ad181fcb25e
Dario2512 <dario.friskan@student.upt.ro>
2023-03-22 17:16:33 +0200

    Addeed SM-2

---
+++ src/loose.oose.fis.lab.student.manager/model/Student.java
@@ -0,0 +1,24 @@
+package loose.oose.fis.lab.student.manager.model;
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+    public Student(String firstName, String lastName, int age,
+                   double meanGrade) {
+//assignments
+    }
+    @Override
+    public String toString() {
+//toString generated implementation
+    }
+    @Override
+    public boolean equals(Object o) {
+//equals generated implementation
+    }
+    @Override
+    public int hashCode() {
+//hashCode generated implementation
+    }
+//generated getters and setters for all fields
+}
